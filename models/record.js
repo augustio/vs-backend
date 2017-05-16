531ff8b2fecc02e17c07635e7d37fe5811a6b758
augustio <augustine.onubeze@metropolia.fi>
2017-05-16 18:42:54 +0300

    First implementation of record model

---
+++ models/record.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const Schema = mongoose.Schema;
+
+const recordSchema = Schema({
+  _id: {type: String, required: true},
+  patientId: {type: String, required: true},
+  type: {type: String, required: true},
+  startTimeStamp: {type: Number, required: true},
+  endTimeStamp: {type: Number},
+  size: {type: Number},
+  sessionConfigParams: {type: {}}
+});
+
+recordSchema.index({timeStamp: 1, patientId: 1});
+recordSchema.index({patientId: 1});
+recordSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('Record', recordSchema);
