c60fa393859838b6cc31b4661cbd546430bb7f4d
augustio <augustine.onubeze@metropolia.fi>
2017-05-17 16:37:37 +0300

    Implement record and auth controllers, first attempt

---
+++ controllers/authController.js
@@ -0,0 +1,11 @@
+const User = require('../models/User');
+
+//Register new user
+exports.register = (req, res, next) => {
+  res.send('NOT IMPLEMENTED: REGISTER');
+};
+
+//User login
+exports.login = (req, res, next) => {
+  res.send('NOT IMPLEMENTED: LOGIN');
+};
---
+++ controllers/recordController.js
@@ -0,0 +1,26 @@
+const Record = require('.../models/record');
+
+//Get list of records
+exports.getRecords = (req, res, next) => {
+  res.send('NOT IMPLEMENTED: GET_RECORDS');
+};
+
+//Get a single records
+exports.getRecord = (req, res, next) => {
+  res.send('NOT IMPLEMENTED: GET_RECORD');
+};
+
+//Handle create record request
+exports.createRecord = (req, res, next) => {
+  res.send('NOT IMPLEMENTED: CREATE_RECORD');
+};
+
+//Handle delete record request
+exports.deleteRecord = (req, res, next) => {
+  res.send('NOT IMPLEMENTED: DELETE_RECORD');
+};
+
+//Handle update record request
+exports.updateRecord = (req, res, next) => {
+  res.send('NOT IMPLEMENTED: UPDATE_RECORD');
+};
