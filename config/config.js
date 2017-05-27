ddad2a36ccd1ad4f63624493496204ad2befd513
augustio <augustine.onubeze@metropolia.fi>
2017-05-27 12:13:19 +0300

    More on setting db URL

--- config/config.js
+++ config/config.js
@@ -1,5 +1,12 @@
 //Application configuration file
 
-exports.dbURL = "mongodb://83.136.249.208:27017/vs_db";
+let database_URL = "mongodb://localhost:27017/vs_db";
+let env = process.env.NODE_ENV;
+
+if(env == 'development'){
+  database_URL = "mongodb://83.136.249.208:27017/vs_db";
+}
+
+exports.dbURL = database_URL;
 
 exports.TOKEN_SECRET = "v1t4753n553cr3tk3y";
